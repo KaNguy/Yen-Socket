38c7837bae33c8650d1ce1470481331caa98c02b
KaNguy <68307546+KaNguy@users.noreply.github.com>
2021-03-01 21:37:23 -0800

    CreateHeaders is now InitializeHeaders

---
+++ src/util/InitializeHeaders.js
@@ -0,0 +1,41 @@
+const { URL } = require('url');
+const { generateSecWebSocketKey } = require('../util/GenerateKey');
+const generatedWSKey = generateSecWebSocketKey(16, "base64");
+
+class InitializeHeaders {
+    constructor(options = {}) {
+        if (options) this.options = options;
+        if (options.URL) {
+            this.options.URL = options.URL;
+        }
+    }
+
+    parseHeaders(url = this) {
+        const theURL = new URL(String(url));
+        const headers = {
+            Host: `${theURL.host}:${theURL.port || 443}`,
+            Connection: "Upgrade",
+            Upgrade: "websocket",
+            "Sec-WebSocket-Key": generatedWSKey,
+            "Sec-WebSocket-Version": "13",
+        };
+
+        return {
+            agent: false,
+            hostname: theURL.hostname,
+            port: theURL.port || 443,
+            method: "GET",
+            path: `${theURL.pathname}${theURL.search}`,
+            headers
+        };
+    }
+
+    get generatedWSKey() {
+        return generatedWSKey;
+    }
+}
+
+module.exports = {
+    InitializeHeaders: InitializeHeaders,
+    generatedWSKey
+};
