69ce381ebb509759a16bea8f8e51078eb0dbc436
KaNguy <68307546+KaNguy@users.noreply.github.com>
2021-03-02 09:29:34 -0800

    Added index.js file

---
+++ src/index.js
@@ -0,0 +1,7 @@
+module.exports = {
+    YenSocket: require('./connect/YenSocket').YenSocket,
+    Constants: require('./constants/Constants'),
+    FrameBuffer: require('./util/FrameBuffer'),
+    GenerateKey: require('./util/GenerateKey'),
+    InitializeHeaders: require('./util/InitializeHeaders')
+}
