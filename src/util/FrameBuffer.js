20eca493ee2b434943c457e3555ea6cda227be98
KaNguy <68307546+KaNguy@users.noreply.github.com>
2021-03-01 21:36:46 -0800

    decodeWebSocketFrame() was refactored to decode() for now. The issue of poorly emitting data has been fixed by adding the socket listener into the function.

--- src/util/FrameBuffer.js
+++ src/util/FrameBuffer.js
@@ -1,57 +1,4 @@
-// NOTE: This function needs some fixing since it seems to decode incoming data poorly when it is called and only decodes roughly half the data.
-function decodeWebSocketFrame(buffer, data, socket) {
-    let framebuffer = null;
-    buffer = Buffer.concat([buffer, data], buffer.length + data.length);
-
-    if (buffer.length > 2) {
-        // buf0
-        const buf0 = buffer[0];
-        const hb = buf0 >> 4;
-        const fin = hb === 8;
-        const opcode = buf0 % 16;
-
-        // buf1
-        const buf1 = buffer[1];
-        const hasMask = buf1 >> 7;
-        let length = buf1 % 128;
-        let start = hasMask ? 6 : 2;
-        if (buffer.length < start + length) return;
-        if (length === 126) {
-            length = buffer.readUInt16BE(2);
-            start += 2;
-        } else if (length === 127) {
-            length = buffer.readUInt32BE(2) * Math.pow(2, 32) + buffer.readUInt32BE(6);
-            start += 8;
-        }
-        if (buffer.length < start + length) return;
-        let payload = buffer.slice(start, start.length);
-        if (hasMask) {
-            const mask = buffer.slice(start - 4, start);
-            for (let index = 0; index < payload.length; index++) {
-                payload['i'] ^= mask['i' % 4];
-            }
-        }
-        buffer = buffer.slice(start + length);
-        if (opcode === 1) {
-            payload = payload.toString();
-            framebuffer = framebuffer ? framebuffer + payload : payload;
-            if (fin) {
-                socket.emit("message", JSON.parse(framebuffer));
-                framebuffer = null;
-            }
-        }
-        if (opcode === 8) {
-            if (payload.length >= 2) {
-                const code = payload.readUInt16BE(0);
-                const reason = payload.slice(2).toString();
-                console.log(`Close ${opcode}`, code, reason);
-            }
-        }
-    }
-}
-
 function generateMeta(fin, op, masked, payload) {
-    // Generates meta based on the op
     const length = payload.length;
     const meta = Buffer.alloc(
         2 + (length < 126 ? 0 : length < 65536 ? 2 : 8) + (masked ? 4 : 0)
@@ -92,8 +39,63 @@ function generateMessage(data) {
     return Buffer.concat([meta, payload], meta.length + payload.length);
 }
 
+const { BASE_BUFFER } = require('../constants/Constants');
+function decode(socket, buffer, frameBuffer) {
+    this.socket = socket;
+    buffer = BASE_BUFFER;
+    frameBuffer = null;
+    this.socket.on('data', data => {
+        buffer = Buffer.concat([buffer, data], buffer.length + data.length);
+
+        if (buffer.length > 2) {
+            const buf0 = buffer[0];
+            const hb = buf0 >> 4;
+            const fin = hb === 8;
+            const opcode = buf0 % 16;
+
+            const buf1 = buffer[1];
+            const hasMask = buf1 >> 7;
+            let length = buf1 % 128;
+            let start = hasMask ? 6 : 2;
+            if (buffer.length < start + length) return;
+            if (length === 126) {
+                length = buffer.readUInt16BE(2);
+                start += 2;
+            } else if (length === 127) {
+                length =
+                    buffer.readUInt32BE(2) * Math.pow(2, 32) + buffer.readUInt32BE(6);
+                start += 8;
+            }
+            if (buffer.length < start + length) return;
+            let payload = buffer.slice(start, start.length);
+            if (hasMask) {
+                const mask = buffer.slice(start - 4, start);
+                for (let index = 0; index < payload.length; index++) {
+                    payload[this.i] ^= mask[this.i % 4];
+                }
+            }
+            buffer = buffer.slice(start + length);
+            if (opcode === 1) {
+                payload = payload.toString();
+                frameBuffer = frameBuffer ? frameBuffer + payload : payload;
+                if (fin) {
+                    // console.log(frameBuffer);
+                    this.socket.emit("message", JSON.parse(frameBuffer));
+                    frameBuffer = null;
+                }
+            }
+            if (opcode === 8) {
+                if (payload.length >= 2) {
+                    const code = payload.readUInt16BE(0);
+                    const reason = payload.slice(2).toString();
+                    console.log(`Close, opcode: ${opcode}`, code, reason);
+                }
+            }
+        }
+    });
+}
+
 module.exports = {
-    decodeWebSocketFrame,
-    generateMeta,
-    generateMessage
+    generateMessage,
+    decode
 }
