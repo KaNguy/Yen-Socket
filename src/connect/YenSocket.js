4fb66e2da84036f162f1c89a1ccd9a101d3dc44f
KaNguy <68307546+KaNguy@users.noreply.github.com>
2021-03-02 21:34:59 -0800

    Added validation factors for the websocket which checks if there is a sec-websocket-accept header and also checks if the server key is valid.

--- src/connect/YenSocket.js
+++ src/connect/YenSocket.js
@@ -3,6 +3,7 @@ const { InitializeHeaders } = require('../util/InitializeHeaders');
 const { EventEmitter } = require('events');
 const { BASE_BUFFER } = require('../constants/Constants');
 const { decode, generateMessage } = require('../util/FrameBuffer');
+const { generateExpectedKey } = require('../util/GenerateKey');
 
 class YenSocket extends EventEmitter {
     constructor(url, options) {
@@ -10,10 +11,17 @@ class YenSocket extends EventEmitter {
         this.url = url;
         this.options = options || {};
 
-        this.request = https.request(new InitializeHeaders().parseHeaders(url));
+        const initializeHeaders = new InitializeHeaders();
+        this.request = https.request(initializeHeaders.parseHeaders(url));
         this.request.end();
 
         this.request.on('upgrade', (response, socket) => {
+            // Check for websocket connection
+            if (!response.headers["sec-websocket-accept"]) throw new Error("The sec-websocket-accept header is missing.");
+            // Validation of the server key
+            const expectedKey = generateExpectedKey("sha1", initializeHeaders.getGeneratedWSKey(), "base64");
+            if (response.headers["sec-websocket-accept"] !== expectedKey) throw new Error("The sec-websocket-accept header returned a mismatched key.");
+
             this.emit('open', ({ response, socket }));
 
             let buffer = BASE_BUFFER;
@@ -37,9 +45,26 @@ class YenSocket extends EventEmitter {
         });
     }
 
+    // Destroys the connection, but may be lossy
     destroy() {
         this.on('open', ({ response, socket }) => {
             return response.destroyed ? null : socket.destroy();
+        });
+    }
+
+    // Closes the connection in a much cleaner & graceful manner
+    // TODO: Finish implementing this, no websocket can recognize this.
+    close() {
+        this.on('open', ({ response, socket}) => {
+            // socket.write(
+            //     generateMessage(
+            //         JSON.stringify({
+            //                 fin: true,
+            //                 opcode: 0x08
+            //             }
+            //         )
+            //     )
+            // );
         });
     }
 }
