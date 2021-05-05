cf8bf4086ef1be065e9c2fc6e6c9cc5c6a44a3ba
KaNguy <68307546+KaNguy@users.noreply.github.com>
2021-05-05 16:29:13 -0700

    Added full validation checks for the WebSocket connection

--- src/connect/YenSocket.js
+++ src/connect/YenSocket.js
@@ -22,6 +22,8 @@ class YenSocket extends EventEmitter {
             const expectedKey = generateExpectedKey("sha1", initializeHeaders.getGeneratedWSKey(), "base64");
             if (response.headers["sec-websocket-accept"] !== expectedKey) throw new Error("The sec-websocket-accept header returned a mismatched key.");
 
+            validateHandshake(response.headers, initializeHeaders.getGeneratedWSKey());
+
             this.emit('open', ({ response, socket }));
             this.destroyed = socket.destroyed;
 
@@ -61,6 +63,37 @@ class YenSocket extends EventEmitter {
             socket.write(close);
         });
     }
+}
+
+/**
+ * Validates the incoming handshake and compares WebSocket keys
+ * @param {IncomingHttpHeaders} handshake - Handshake from the return response
+ * @param {string} wsKey - The WebSocket key
+ * @returns {boolean} - Sole purpose is to validate the handshake, not return values, but it will return true if it is required for other checks
+ */
+const validateHandshake = function(handshake, wsKey) {
+    let headers = handshake, key;
+
+    if (handshake.length < 4) {
+        throw new Error("Invalid handshake, the handshake was too small.");
+    }
+
+    if (!headers['upgrade'] || !headers['sec-websocket-accept'] || !headers['connection']) {
+        throw new Error("Invalid handshake, required header(s) are missing.");
+    }
+
+    if (headers['upgrade']?.toLowerCase() !== "websocket" || headers['connection']?.toLowerCase().split(/\s*,\s*/).indexOf('upgrade') === -1) {
+        throw new Error("Invalid handshake, invalid Upgrade/Connection header(s).");
+    }
+
+    // Validate the server key
+    key = headers['sec-websocket-accept'];
+    const expectedKey = generateExpectedKey("sha1", wsKey, "base64");
+    if (key !== expectedKey) {
+        throw new Error("The sec-websocket-accept header returned a mismatched key.");
+    }
+
+    return true;
 }
 
 module.exports = {
