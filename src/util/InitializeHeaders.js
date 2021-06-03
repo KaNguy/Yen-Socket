4b9ffeb764f3cbbcf1e59d6ee2321f0e70807b7f
KaNguy <68307546+KaNguy@users.noreply.github.com>
2021-06-02 17:06:36 -0700

    Refactor, change parameter

--- src/util/InitializeHeaders.js
+++ src/util/InitializeHeaders.js
@@ -4,14 +4,13 @@ const { generateSecWebSocketKey } = require('../util/GenerateKey');
 class InitializeHeaders {
     constructor(options = {}) {
         if (options) this.options = options;
-        if (options.URL) {
-            this.options.URL = options.URL;
-        }
+        this.options.URL = options.URL ? options.URL : null;
+
 
         this.generatedWSKey = generateSecWebSocketKey(16, "base64");
     }
 
-    parseHeaders(url = this) {
+    parseHeaders(url = this.options.URL) {
         const theURL = new URL(String(url));
         const headers = {
             Host: `${theURL.host}:${theURL.port || 443}`,
